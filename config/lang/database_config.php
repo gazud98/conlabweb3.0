a5f54bc660ce129ab8b37f1e0aabd6fac1b4796d
gazud <eduardo.gazud@gmail.com>
2024-04-30 10:06:12 -0500

    database_config

--- config/lang/database_config.php
+++ config/lang/database_config.php
@@ -1,7 +1,7 @@
 <?php  
 
         define('CONEXIONDBACCESS', array( "driver"    => "mysql",
-                                          "host"      => "localhost",
+                                          "host"      => "185.239.210.103",
                                           "user"      => "u116753122_erbin",
                                           "pass"      => "3012046491@Erbin",
                                           "database"  => prefixdb,
