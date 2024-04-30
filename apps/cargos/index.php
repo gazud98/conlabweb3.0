6ae76ee3dcf404f01412bd1ee76cd50c0721c6c0
erbin20 <erbinantonio20@gmail.com>
2024-04-30 14:33:06 -0500

    Update index.php

--- apps/cargos/index.php
+++ apps/cargos/index.php
@@ -14,11 +14,11 @@ $uppercaseruta = strtoupper($ruta);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
-    <link rel="stylesheet" href="/cw3/conlabweb3.0/apps/cargos/assets/style.css">
+    <link rel="stylesheet" href="https://conlabweb3.tierramontemariana.org/apps/cargos/assets/style.css">
 </head>
 <style>
     .content-wrapper {
-        background-image: url('/cw3/conlabweb3.0/apps/medicos/assets/backcw3-v1.png');
+        background-image: url('https://conlabweb3.tierramontemariana.org/apps/medicos/assets/backcw3-v1.png');
         background-size: cover;
         background-repeat: no-repeat;
     }
